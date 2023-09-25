905c02f97c3a7853d2fff19ec328d033d9cbe788
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-09-25 15:56:29 +0800

    Create dore.js

---
+++ scripts/dore.js
@@ -0,0 +1,10 @@
+// this is diamond ore Mod planet generator. Original Mod: Diamond Ore Mode by Z0mbiesrock. 
+
+function init_random() // randomises each time you visit.  
+{
+  
+}
+
+var doregen = extend(SerpuloPlanetGenerator,{
+  
+}) 
